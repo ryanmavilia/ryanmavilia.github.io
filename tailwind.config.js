9ead75ef2bfc5bfa5d2266b16eacda02180da089
Ryan Mavilia <ryanmavilia@gmail.com>
2024-03-20 17:20:42 -0400

    Scripts cleanup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          blue: {
+            light: "#00ccdd"
+          }
+        }
+      }
+    }
+  },
+  darkMode: "class"
+};
---
+++ theme.check.js
@@ -0,0 +1,7 @@
+if (localStorage.getItem('dark-mode') === 'true' || (!('dark-mode' in localStorage) &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.querySelector('html').classList.add('dark');
+} else {
+  document.querySelector('html').classList.remove('dark');
+}
+
