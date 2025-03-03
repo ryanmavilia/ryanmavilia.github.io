5f85b69e841af79f6f18acdc5a4e78c239b4dfe7
Ryan Mavilia <ryanmavilia@gmail.com>
2025-03-02 20:35:02 -0800

    New favicon

--- src/lib/featured.ts
+++ src/lib/featured.ts
@@ -6,7 +6,7 @@ export const featuredProjects = (
     "projects",
     (data) => {
       const shortDescription = getShortDescription(
-        data.frontmatter.description,
+        data.frontmatter.description
       );
       return {
         title: data.frontmatter.title,
@@ -18,7 +18,7 @@ export const featuredProjects = (
         timestamp: data.frontmatter.timestamp,
         filename: `/projects/${data.frontmatter.filename}`,
       };
-    },
+    }
   )
 )
   .filter((project) => project.featured)
@@ -29,27 +29,28 @@ export const featuredProjects = (
   });
 
 export const featuredArticles = (
-    await processContentInDir<ArticleFrontmatter, ArticleFrontmatter>(
-      "blog",
-      (data) => {
-        const shortDescription = getShortDescription(
-          data.frontmatter.description,
-        );
-        return {
-          title: data.frontmatter.title,
-          description: shortDescription,
-          tags: data.frontmatter.tags,
-          time: data.frontmatter.time,
-          featured: data.frontmatter.featured,
-          timestamp: data.frontmatter.timestamp,
-          filename: `/blog/${data.frontmatter.filename}`,
-        };
-      },
-    )
+  await processContentInDir<ArticleFrontmatter, ArticleFrontmatter>(
+    "blog",
+    (data) => {
+      const shortDescription = getShortDescription(
+        data.frontmatter.description
+      );
+      return {
+        title: data.frontmatter.title,
+        description: shortDescription,
+        tags: data.frontmatter.tags,
+        time: data.frontmatter.time,
+        featured: data.frontmatter.featured,
+        timestamp: data.frontmatter.timestamp,
+        filename: `/blog/${data.frontmatter.filename}`,
+      };
+    }
   )
-    .filter((project) => project.featured)
-    .sort((a, b) => {
-      const dateA = new Date(a.timestamp);
-      const dateB = new Date(b.timestamp);
-      return dateB.getTime() - dateA.getTime();
-    });
+)
+  .filter((project) => project.featured)
+  .sort((a, b) => {
+    const dateA = new Date(a.timestamp);
+    const dateB = new Date(b.timestamp);
+    return dateB.getTime() - dateA.getTime();
+  })
+  .slice(0, 3);
